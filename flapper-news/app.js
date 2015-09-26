12a6e7e6591fc9cdd2c888c70e93c19c7808246f
Sasha Mihalache <sasha.mihalache@gmail.com>
2015-09-26 18:34:13 +0300

    Added token-based authentication with Passport. Small issues when logging out.

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose =  require('mongoose');
-                require("./models/Posts");
-                require("./models/Comments");
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var bodyParser   = require('body-parser');
+var mongoose     = require('mongoose');
+                   require("./models/Posts");
+                   require("./models/Comments");
+                   require("./models/Users");
+                   require('./config/passport');
+var routes       = require('./routes/index');
+var users        = require('./routes/users');
+var passport     = require('passport');
 
 var app = express();
 
@@ -27,6 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules'));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
